59a34098430e11aebcb93d3db78314a126c7ce61
Binvnhehe999 <104841668+Binvnhehe999@users.noreply.github.com>
2025-04-01 11:11:26 +0000

    Pending changes exported from your codespace

---
+++ index2.php
@@ -0,0 +1,11 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require __DIR__ . '/vendor/autoload.php'; // Load thư viện
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__/assets/);
+$dotenv->load();
+
+echo 'DB_HOST: ' . $_ENV['DB_HOST'];
+?>
